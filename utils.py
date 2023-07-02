5e52b0016eb835cd5d136088a8c1015200d761d7
kushthakker <kush.thakker@galaxeye.space>
2023-07-02 14:20:30 +0530

    added comments

--- utils.py
+++ utils.py
@@ -1,44 +1,48 @@
 import os
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] ='./auth.json'
-from google.cloud import vision
-import re
 from PIL import Image
-vision_client = vision.ImageAnnotatorClient()
-import json
-import re
-from google.cloud import vision
-from google.cloud import storage
-import pypdfium2 as pdfium
-import time
-from pytesseract import Output
 import pytesseract
 import cv2
 import threading
 from prettytable import PrettyTable
 from tqdm import tqdm
 import time
+from pytesseract import Output
+from google.cloud import vision
+from google.cloud import storage
+import pypdfium2 as pdfium
+
+# Set Google Cloud credentials
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './auth.json'
+
+# Initializing Google Vision API client
+vision_client = vision.ImageAnnotatorClient()
 
 
 def convert_to_images(pdf_path):
+    # Convert each page of the PDF to images
     pdf = pdfium.PdfDocument(pdf_path)
     n_pages = len(pdf)
-    loading_bar = tqdm(total=n_pages, desc="Generating images from pdf")
+    loading_bar = tqdm(total=n_pages, desc="Generating images from PDF")
 
     for i in range(len(pdf)):
-        filename = str(i)+".png"
+        filename = str(i) + ".png"
         page = pdf.get_page(i)
-        pil_image = page.render(scale = 300/72).to_pil()
+        pil_image = page.render(scale=300/72).to_pil()
         pil_image.save(f"./output/{filename}")
         loading_bar.update(1)
     loading_bar.close()
 
+
 def detect_text_from_page_google_vision(path_to_images):
-    response_dictionary = {"pages":[]}
+    # Perform text detection using Google Vision API
+    response_dictionary = {"pages": []}
     text_on_page = []
     time_taken_per_page = []
     total_time = time.time()
+
     for path_to_image in path_to_images:
         file_name = os.path.abspath(path_to_image)
+
         with open(file_name, "rb") as image_file:
             content = image_file.read()
 
@@ -51,28 +55,35 @@ def detect_text_from_page_google_vision(path_to_images):
         response = response.text_annotations[0].description
 
         text_on_page.append(response)
-        time_taken_per_page.append(endtime-starttime)
-    totaltime= time.time() - total_time
+        time_taken_per_page.append(endtime - starttime)
+
+    totaltime = time.time() - total_time
+
     for i in range(0, len(text_on_page)):
-        dummy_dictionary = {"page":i+1,
-                            "page_text":text_on_page[i],
-                            "page_time":time_taken_per_page[i]}
+        dummy_dictionary = {
+            "page": i + 1,
+            "page_text": text_on_page[i],
+            "page_time": time_taken_per_page[i]
+        }
         response_dictionary["pages"].append(dummy_dictionary)
+
     response_dictionary["total_time"] = totaltime
     return response_dictionary
 
+
 def detect_text_from_page_tesseract_single_thread(path_to_images):
+    #text detection using Tesseract OCR (single-threaded)
     loading_bar = tqdm(total=len(path_to_images), desc="Processing Images")
-    response_dictionary = {"pages":[]}
+    response_dictionary = {"pages": []}
     text_on_page = []
     time_taken_per_page = []
     total_time = time.time()
+
     for path_to_image in path_to_images:
         file_name = os.path.abspath(path_to_image)
 
         image = cv2.imread(file_name)
         rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        
 
         starttime = time.time()
         results = pytesseract.image_to_data(rgb, output_type=Output.DICT)
@@ -80,19 +91,24 @@ def detect_text_from_page_tesseract_single_thread(path_to_images):
 
         response = ""
         for result in results["text"]:
-            response = response + " " + result 
+            response = response + " " + result
 
         text_on_page.append(response)
-        time_taken_per_page.append(endtime-starttime)
+        time_taken_per_page.append(endtime - starttime)
         loading_bar.update(1)
-    totaltime= time.time() - total_time
+
+    totaltime = time.time() - total_time
+
     for i in range(0, len(text_on_page)):
-        dummy_dictionary = {"page":i+1,
-                            "page_text":text_on_page[i],
-                            "page_time":time_taken_per_page[i]}
+        dummy_dictionary = {
+            "page": i + 1,
+            "page_text": text_on_page[i],
+            "page_time": time_taken_per_page[i]
+        }
         response_dictionary["pages"].append(dummy_dictionary)
-    response_dictionary["total_time"] = totaltime
+
     loading_bar.close()
+
     table = PrettyTable()
     for i in range(len(time_taken_per_page)):
         just_time = {"page": i + 1, "page_time": time_taken_per_page[i]}
@@ -103,7 +119,9 @@ def detect_text_from_page_tesseract_single_thread(path_to_images):
     print(table.get_string(border=True, padding_width=2))
     return response_dictionary
 
+
 def detect_text_from_page_tesseract_multi_thread(path_to_images):
+    # Perform text detection using Tesseract OCR (multi-threaded)
     response_dictionary = {"pages": []}
     text_on_page = [None] * len(path_to_images)
     time_taken_per_page = [None] * len(path_to_images)
@@ -134,6 +152,7 @@ def detect_text_from_page_tesseract_multi_thread(path_to_images):
             pbar.update(1)
 
     totaltime = time.time() - total_time
+
     for i in range(0, len(text_on_page)):
         dummy_dictionary = {
             "page": i + 1,
@@ -141,8 +160,7 @@ def detect_text_from_page_tesseract_multi_thread(path_to_images):
             "page_time": time_taken_per_page[i]
         }
         response_dictionary["pages"].append(dummy_dictionary)
-    response_dictionary["total_time"] = totaltime
-
+    # creating table to print to console
     table = PrettyTable()
     table.field_names = ["Pages", "Total time (sec)"]
     table.add_row([len(text_on_page), totaltime])
